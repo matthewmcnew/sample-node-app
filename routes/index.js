e1996c52a3a152c3742b8bde2bb0d275321bbdec
matthewmcnew <me@mattmcnew.com>
2019-05-10 22:31:02 -0500

    Add security check

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+const fs = require('fs')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chicago Code Camp' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Chicago Code Camp'});
+});
+
+
+/* GET home page. */
+router.get('/security', function (req, res, next) {
+    try {
+        if (fs.existsSync("/home/pack/fix.txt")) {
+            res.render('index', {title: 'All fixed!'});
+            //file exists
+        } else {
+            res.render('index', {title: 'There is a security vulnerability!'});
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
 });
 
 module.exports = router;
